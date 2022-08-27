292e81189dbe8869d8cb5611044e5bbb1c2d2a5e
nsingla20 <nsingla20@iitk.ac.in>
2022-08-27 17:55:14 +0530

    update q1

--- HW1/Q1.hs
+++ HW1/Q1.hs
@@ -1,7 +1,7 @@
 takeAlternate :: Int->[a]->[a]
 takeAlternate 0 _ = []
 takeAlternate _ [] = []
-takeAlternate n [x,_] = [x]
-takeAlternate n [x] = [x]
+takeAlternate _ [x] = [x]
+takeAlternate _ [x,_] = [x]
 takeAlternate n (x:(y:xs)) = x:takeAlternate (n-1) xs
 
